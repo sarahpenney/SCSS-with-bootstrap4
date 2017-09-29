48e5412469489e4549a529e750f599eed3754388
Sarah Penney <sarahpenneyuk@gmail.com>
2017-09-29 15:33:47 +0100

    ADDED: begining of JSON file for dummy cms data

--- js/app.js
+++ js/app.js
@@ -2,20 +2,28 @@
 
 var mumsnetApp = angular.module('mumsnetApp', ['ngRoute']);
 
-mumsnetApp.controller('mainController', function($scope) {
+mumsnetApp.controller('mainController', function($scope, $http) {
   $scope.message = 'testing';
 
+  // DUMMY CONTENT DATA
+    $scope.talklinks = [
+        {title: 'August vs September birth (schools)', count: '856'},
+        {title: 'Scared', count: '6'},
+        {title: 'First Time Pregnancy (2)', count: '300'},
+        {title: 'Please help!!!!', count: '25'},
+        {title: 'First Time Pregnancy (2)', count: '300'}
+    ];
 
-// DUMMY CONTENT DATA
-  $scope.talklinks = [
-      {title: 'August vs September birth (schools)', count: '856'},
-      {title: 'Scared', count: '6'},
-      {title: 'First Time Pregnancy (2)', count: '300'},
-      {title: 'Please help!!!!', count: '25'},
-      {title: 'First Time Pregnancy (2)', count: '300'}
-  ];
-
-
+// FOOTER DATA COMING IN FROM JSON FILE
+  $http.get('/cms-dummy-data.json')
+            .success(function (data) {
+              $scope.data = data.array;
+              $scope.title = data.title;
+              $scope.subtitles = data.subtitles;
+            })
+            .error(function (data) {
+              console.log('err');
+            });
 });
 
 
